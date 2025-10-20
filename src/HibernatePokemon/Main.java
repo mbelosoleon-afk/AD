0d970031f6eebf544ee70ee20eafad1b3a498020
dam <mbelosoleon@danielcastelao.org>
2025-10-20 10:14:33 +0200

    Clase, Config y Metodos Pokedex parte1

--- src/HibernatePokemon/Main.java
+++ src/HibernatePokemon/Main.java
@@ -3,6 +3,6 @@ package HibernatePokemon;
 public class Main {
     public static void main(String[] args) {
         //Insertamos los pokemons en la tabla Pokédex
-        PokedexService.crearPokemon("Luxray",42,"")
+        PokedexService.crearPokemon("Luxray",42,"");
     }
 }
